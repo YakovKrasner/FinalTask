d0585efd6d69dfa519150b7b97337a4d3c3e8fe8
Yakov Krasner <yakov.krasner@yandex.ru>
2022-09-16 20:04:37 +0700

    Programm added

---
+++ SelectArrayWithThreeSymbols/Program.cs
@@ -0,0 +1,19 @@
+﻿string [] PrimaryArray = {"Hola, muchachos!","Feci quod potui, faciant meliora potentes!",":)","Yes"};
+int MaxStringLength = 3; // Указываем максимальное количество знаков, при котором мы будем включаь элемент исходного массива в новый массив
+int DerivateArrayElements = 0; // Вводим счетчик для количества элементов, содержащих MaxStringLength символов или менее.
+for (int i = 0; i < PrimaryArray.Length; i++) 
+    {
+        if (PrimaryArray[i].Length<=MaxStringLength) DerivateArrayElements ++;
+    }
+string [] DerivateArray = new string [DerivateArrayElements];
+
+int j = 0;
+for (int i = 0; i < PrimaryArray.Length; i++) 
+    {
+        if (PrimaryArray[i].Length<=MaxStringLength) 
+                {
+                    DerivateArray [j] = PrimaryArray [i];
+                    j ++;
+                }
+    }
+for (int i = 0; i < DerivateArray.Length; i++) Console.WriteLine (DerivateArray[i]);
